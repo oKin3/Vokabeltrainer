bf198ed1ee3e64095820f3a0b0c072b0537436ce
Niko Reichardt <niko.reichardt@gmail.com>
2015-10-21 23:36:29 +0200

    Add Userlogin as Facade

---
+++ src/java/ch/nihongo/vokabeltrainer/facade/UserloginFacade.java
@@ -0,0 +1,35 @@
+package ch.nihongo.vokabeltrainer.facade;
+
+import ch.nihongo.vokabeltrainer.dao.UserloginDAO;
+import ch.nihongo.vokabeltrainer.entities.Userlogin;
+import org.apache.commons.codec.digest.Crypt;
+
+/**
+ *
+ * @author Niko Reichardt
+ */
+public class UserloginFacade {
+
+    private UserloginDAO userDAO = new UserloginDAO();
+
+    public boolean isUsernameNotExist(String username) {
+        userDAO.beginTransaction();
+        Userlogin user = userDAO.findByUsername(username);
+
+        return user == null;
+    }
+
+    public boolean isEmailNotExist(String email) {
+        userDAO.beginTransaction();
+        Userlogin user = userDAO.findByEmail(email);
+
+        return user == null;
+    }
+
+    public boolean isPasswordCorrect(String username, String password) {
+        userDAO.beginTransaction();
+        Userlogin user = userDAO.findByUsername(username);
+
+        return user.getPassword().equals(Crypt.crypt(password, user.getPassword()));
+    }
+}
