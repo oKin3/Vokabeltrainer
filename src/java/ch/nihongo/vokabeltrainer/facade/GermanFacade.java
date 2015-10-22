776980a83129974bebcc1ca0f1a5fad6c5b50cf9
Niko Reichardt <niko.reichardt@gmail.com>
2015-10-22 21:42:33 +0200

    Add German as Facade

---
+++ src/java/ch/nihongo/vokabeltrainer/facade/GermanFacade.java
@@ -0,0 +1,26 @@
+package ch.nihongo.vokabeltrainer.facade;
+
+import ch.nihongo.vokabeltrainer.dao.GermanDAO;
+import ch.nihongo.vokabeltrainer.entities.German;
+import java.util.List;
+
+/**
+ *
+ * @author Niko Reichardt
+ */
+public class GermanFacade {
+
+    private GermanDAO germanDAO;
+
+    public GermanFacade() {
+        germanDAO = new GermanDAO();
+    }
+
+    public List<German> findAll() {
+        germanDAO.beginTransaction();
+        List<German> result = germanDAO.findAll();
+        germanDAO.closeTransaction();
+
+        return result;
+    }
+}
