d20f0eb1341210629c77d4f72625521940eae0e7
Niko Reichardt <niko.reichardt@gmail.com>
2015-10-22 21:42:50 +0200

    Add Japanese as Facade

---
+++ src/java/ch/nihongo/vokabeltrainer/facade/JapaneseFacade.java
@@ -0,0 +1,26 @@
+package ch.nihongo.vokabeltrainer.facade;
+
+import ch.nihongo.vokabeltrainer.dao.JapaneseDAO;
+import ch.nihongo.vokabeltrainer.entities.Japanese;
+import java.util.List;
+
+/**
+ *
+ * @author Niko Reichardt
+ */
+public class JapaneseFacade {
+
+    private JapaneseDAO japaneseDAO;
+
+    public JapaneseFacade() {
+        japaneseDAO = new JapaneseDAO();
+    }
+
+    public List<Japanese> findAll() {
+        japaneseDAO.beginTransaction();
+        List<Japanese> result = japaneseDAO.findAll();
+        japaneseDAO.closeTransaction();
+
+        return result;
+    }
+}
