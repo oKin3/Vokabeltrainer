2f324056b429de1e70cfe40d3f5ffa9f7b9d8ed0
Niko Reichardt <niko.reichardt@gmail.com>
2015-10-22 21:43:05 +0200

    Add Translation as Facade

---
+++ src/java/ch/nihongo/vokabeltrainer/facade/TranslationFacade.java
@@ -0,0 +1,36 @@
+package ch.nihongo.vokabeltrainer.facade;
+
+import ch.nihongo.vokabeltrainer.dao.GermanDAO;
+import ch.nihongo.vokabeltrainer.dao.JapaneseDAO;
+import ch.nihongo.vokabeltrainer.entities.German;
+import ch.nihongo.vokabeltrainer.entities.Japanese;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Niko Reichardt
+ */
+public class TranslationFacade {
+
+    private GermanDAO germanDAO;
+    private JapaneseDAO japaneseDAO;
+
+    public TranslationFacade() {
+        germanDAO = new GermanDAO();
+        japaneseDAO = new JapaneseDAO();
+    }
+
+    public void createTranslation(Japanese japanese, German german) {
+        japaneseDAO.beginTransaction();
+        germanDAO.beginTransaction();
+        japaneseDAO.persist(japanese);
+        List<Japanese> list = new ArrayList();
+        list.add(japanese);
+        german.setJapaneseList(list);
+        germanDAO.persist(german);
+        japaneseDAO.commitAndCloseTransaction();
+        germanDAO.commitAndCloseTransaction();
+    }
+
+}
