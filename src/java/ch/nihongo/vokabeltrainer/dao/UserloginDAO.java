05c4d37790306ccf5e4c67102d4ad536f9fc7849
Niko Reichardt <niko.reichardt@gmail.com>
2015-10-26 08:13:26 +0100

    Fixed a bug

--- src/java/ch/nihongo/vokabeltrainer/dao/UserloginDAO.java
+++ src/java/ch/nihongo/vokabeltrainer/dao/UserloginDAO.java
@@ -2,6 +2,7 @@ package ch.nihongo.vokabeltrainer.dao;
 
 import ch.nihongo.vokabeltrainer.entities.Userlogin;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,19 +16,23 @@ public class UserloginDAO extends GenericDAO<Userlogin> {
     public UserloginDAO() {
         super(Userlogin.class);
     }
-    
-    public Userlogin findByEmail(String email){
+
+    public Userlogin findByEmail(String email) {
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put("email", email);     
+        parameters.put("email", email);
 
         return super.findOneResult(Userlogin.FIND_BY_EMAIL, parameters);
     }
-    
-    public Userlogin findByUsername(String username){
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("username", username);     
 
-        return super.findOneResult(Userlogin.FIND_BY_USERNAME, parameters);
+    public Userlogin findByUsername(String username) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("username", username);
+        Userlogin foundEntity = null;
+        List<Userlogin> results = super.findAllResults(Userlogin.FIND_BY_USERNAME, parameters);
+        if (!results.isEmpty()) {
+            foundEntity = results.get(0);
+        }
+        return foundEntity;
     }
 
 }
