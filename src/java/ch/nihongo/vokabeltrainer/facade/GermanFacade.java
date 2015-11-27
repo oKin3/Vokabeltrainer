60a6528f42cc18a2f7fa2aaadfc16c63ebe2237a
Niko Reichardt <niko.reichardt@gmail.com>
2015-11-27 23:15:30 +0100

    Add findByWord and findByCategoryMax

--- src/java/ch/nihongo/vokabeltrainer/facade/GermanFacade.java
+++ src/java/ch/nihongo/vokabeltrainer/facade/GermanFacade.java
@@ -17,17 +17,30 @@ public class GermanFacade {
     }
 
     public List<German> findAll() {
-        germanDAO.beginTransaction();
+        germanDAO.createEntityManager();
         List<German> result = germanDAO.findAll();
-        germanDAO.closeTransaction();
-
+        germanDAO.closeEntityManager();
         return result;
     }
 
     public List<German> findByCategory(String category) {
         germanDAO.createEntityManager();
-        List<German> list = germanDAO.findByCategory(category);
+        List<German> list = germanDAO.findByCategory(category.toUpperCase());
         germanDAO.closeEntityManager();
         return list;
+    }
+    
+    public List<German> findByCategoryMax(String category, int max) {
+        germanDAO.createEntityManager();
+        List<German> list = germanDAO.findByCategoryMax(category, max);
+        germanDAO.closeEntityManager();
+        return list;
+    }
+    
+    public German findByWord(String word) {
+        germanDAO.createEntityManager();
+        German german = germanDAO.findByWord(word);
+        germanDAO.closeEntityManager();
+        return german;
     }
 }
