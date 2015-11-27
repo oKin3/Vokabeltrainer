8282cdb14be267b89fda67a1b82a64b72c8bd6e7
Niko Reichardt <niko.reichardt@gmail.com>
2015-11-27 23:13:48 +0100

    Add Userlogin Model

---
+++ src/java/ch/nihongo/vokabeltrainer/model/UserloginData.java
@@ -0,0 +1,46 @@
+package ch.nihongo.vokabeltrainer.model;
+
+/**
+ *
+ * @author Niko Reichardt
+ */
+public class UserloginData {
+
+    private String username;
+    private String email;
+    private String password;
+    private String passwordConfirmation;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+
+}
