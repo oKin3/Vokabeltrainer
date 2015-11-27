4cae63a26ef97acfa180e23256f0a2392a7cde60
Niko Reichardt <niko.reichardt@gmail.com>
2015-11-27 06:54:12 +0100

    Add new Categories

--- src/java/ch/nihongo/vokabeltrainer/entities/Category.java
+++ src/java/ch/nihongo/vokabeltrainer/entities/Category.java
@@ -7,10 +7,12 @@ package ch.nihongo.vokabeltrainer.entities;
 public enum Category {
 
     FRUIT("Fruit"),
+    VEGETABLES("Vegetables"),
     MEDICAL("Medical"),
     FOOD("Food"),
-    ANIMAL("Animal");
-    
+    ANIMAL("Animal"),
+    TRANSPORT("Transport");
+
     private String name;
 
     private Category(String name) {
